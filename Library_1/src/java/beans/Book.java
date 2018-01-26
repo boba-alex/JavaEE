52646305a565a2d53790e3be8a7edd3e2b08d099
bobaalex <099@tut.by>
2018-01-26 21:46:23 +0200

    only english in database; find by word by genre by choosing author/name by letter

--- Library_1/src/java/beans/Book.java
+++ Library_1/src/java/beans/Book.java
@@ -13,7 +13,7 @@ public class Book implements Serializable {
     private String isbn;
     private String genre;
     private String author;
-    private Date publishDate;
+    private int publishDate;
     private String publisher;
     private byte[] image;
 
@@ -73,11 +73,11 @@ public class Book implements Serializable {
         this.author = author;
     }
 
-    public Date getPublishDate() {
+    public int getPublishDate() {
         return publishDate;
     }
 
-    public void setPublishDate(Date publishDate) {
+    public void setPublishDate(int publishDate) {
         this.publishDate = publishDate;
     }
 
